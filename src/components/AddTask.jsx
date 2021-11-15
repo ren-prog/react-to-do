55b69f0b42ce1ddb0d09ea8bfd48511f66dd78bc
Renan <prog.renan@gmail.com>
2021-11-14 23:20:24 -0300

    new component AddTask

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,14 @@
+import "./AddTask.css";
+import Button from "./Button";
+const AddTask = () => {
+  return (
+    <div className="add-task-container">
+      <input className="add-task-input" type="text" />
+      <div className="add-task-button-container">
+        <Button>Adicionar</Button>
+      </div>
+    </div>
+  );
+};
+
+export default AddTask;
