ed9453998470fc25ce4bb5aa7d3c060eae3f03c2
Renan <prog.renan@gmail.com>
2021-11-15 21:57:15 -0300

    add icons and route details

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-
+import { CgClose, CgInfo } from "react-icons/cg";
+import { useHistory } from "react-router-dom";
 import "./Item.css";
 
 const Item = ({ task, handleTaskClick, handleDeleteTask }) => {
+  const history = useHistory();
+  const handletaskDetailsRoute = () => {
+    history.push(`/details/${task.title}`);
+  };
   return (
     <div className="task-container" style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}>
       <div className="task-title" onClick={() => handleTaskClick(task.id)}>
@@ -11,7 +16,10 @@ const Item = ({ task, handleTaskClick, handleDeleteTask }) => {
 
       <div className="buttons-container">
         <button className="remove-task-button" onClick={() => handleDeleteTask(task.id)}>
-          X
+          <CgClose />
+        </button>
+        <button className="see-task-details-button" onClick={handletaskDetailsRoute}>
+          <CgInfo />
         </button>
       </div>
     </div>
